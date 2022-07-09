e48c5242486e3b8b310ddce7038baacd024adc5c
martinmoragab <martinmoraga23@gmail.com>
2022-07-09 11:45:42 -0700

    Create feedback, product, and user models

---
+++ src/models/feedback.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  }
+})
+
+const feedbackSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: [true, 'Category is required'],
+    enum: ['UI', 'UX', 'Enhancement', 'Bug', 'Feature']
+  },
+  details: {
+    type: String,
+    required: [true, 'Details are required'],
+    trim: true,
+  },
+  status: {
+    // can be Planned, In Progress, Live
+    type: String,
+    enum: ['Not Started', 'Planned', 'In Progress', 'Live'],
+    default: 'Not Started',
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  comments: [{
+    type: commentSchema,
+  }],
+}, { timestamps: true });
+
+const Feedback = mongoose.Model('Feedback', feedbackSchema);
+
+module.exports = Feedback;
---
+++ src/models/product.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Feedback = require('./feedback');
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Product\'s name is required'],
+    trim: true,
+  },
+  owners: [{
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  }],
+  feedbacks: [{
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Feedback',
+  }],
+}, { timestamps: true });
+
+// Remove all tasks related to this product when deleting it
+productSchema.pre('remove', async function (next) {
+  const product = this;
+  await Feedback.deleteMany({ product: product._id });
+  next();
+});
+
+const Product = mongoose.Model('Product', productSchema);
+
+module.exports = Product;
